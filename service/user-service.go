f0c7fe44fecab14be46e6332880dccf661e2135f
zhuliminl <zhuliminl@gmail.com>
2022-11-26 00:53:33 +0800

    下线

--- service/user-service.go
+++ service/user-service.go
@@ -1,11 +1,27 @@
 package service
 
-import "github.com/zhuliminl/easyrn-server/dto"
+import (
+	"github.com/zhuliminl/easyrn-server/dto"
+	"github.com/zhuliminl/easyrn-server/repository"
+)
 
 type UserService interface {
 	GetUserById(id string) (dto.User, error)
 }
 
 type userService struct {
-	// userService
+	userRepository repository.UserRepository
+}
+
+func (u userService) GetUserById(id string) (dto.User, error) {
+	var user dto.User
+	user.Name = "saul"
+	if err := 
+	return user, nil
+}
+
+func NewUserService(userRepo repository.UserRepository) UserService {
+	return &userService{
+		userRepository: userRepo,
+	}
 }
